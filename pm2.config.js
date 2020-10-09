80840e4e9fed17937920f0c3c3cc8239fc33c664
grissomshen <grissomshen@tencent.com>
2020-10-09 20:59:52 +0800

    up

--- pm2.config.js
+++ pm2.config.js
@@ -5,7 +5,7 @@ module.exports = {
     watch : true,
     env: {
       NODE_ENV: "development",
-      DEBUG: "*",
+      //DEBUG: "*",
     },
   }]
 };
--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,27 @@
 
+let curr_x = 0
+let curr_y = 0
+let name = "grissomshen"
+
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
+
+const getName = () => {
+  name = getUrlParameter("name")	
+};
+
 const getImgPos = (e) => {
     console.log(e)
     const touch = e.touches[0]
@@ -7,20 +30,39 @@ const getImgPos = (e) => {
     pageY = e.touches[0].pageY
     console.log(pageX,pageY)
     img_dom = e.target
-    img_x_perc = (pageX - img_dom.offsetLeft) / img_dom.width
-    img_y_perc = (pageY - img_dom.offsetTop) / img_dom.height
-    console.log("x",img_x_perc);
-    console.log("y",img_y_perc);
+    parent_dom = img_dom.offsetParent
+    curr_x = (pageX - parent_dom.offsetLeft) / img_dom.width * 100
+    curr_y = (pageY - parent_dom.offsetTop) / img_dom.height * 100
+    console.log("x",curr_x);
+    console.log("y",curr_y);
+    
+    const table_img = document.getElementById('img_table')
+    table_img.style.left = `${curr_x}%`
+    table_img.style.top = `${curr_y}%`
+};
+
+const onConfirmBtn = (e) => {
+   const obj = {'x': curr_x,'y': curr_y,'n':name}
+   $.ajax({
+     type : "POST",
+     url : "/sit",
+     data : JSON.stringify(obj),
+     contentType: "application/json; charset=utf-8",
+   })
 };
 
 const registerEvent = () => {
-  console.log("registerEvent");
-  const main_div = document.getElementById('main')
+  getName()
+
+  const main_div = document.getElementById('img_bg')
   main_div.addEventListener("touchstart", (e) => {
     getImgPos(e)
   })
-}
 
-console.log("111");
+  const confirm_btn = document.getElementById('confirm_btn')
+  confirm_btn.addEventListener("click", (e) => {
+    onConfirmBtn(e)
+  })
+}
 
 window.onload = registerEvent
